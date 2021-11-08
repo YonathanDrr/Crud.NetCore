6fd1a31002c01381bc701a64fc274b8b01ce2a08
YonathanDrr <yonathandavid.lr96@gmail.com>
2021-11-08 14:37:53 -0300

    primer commit de crudApp 1.0

---
+++ CrudApp/Models/Libro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApp.Models
+{
+    public class Libro 
+    {
+        [Key]
+        public int Id{ get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [StringLength(50, ErrorMessage ="Campo obligatorio", MinimumLength = 3)]
+        [Display (Name = "Titulo")]
+        public string Titulo { get; set; }
+        
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [StringLength(50, ErrorMessage = "Campo obligatorio", MinimumLength = 3)]
+        [Display(Name = "Descripcion")]
+        public string Descripcion { get; set; }
+
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Lanzamiento")]
+        public DataType FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage ="Campo Obligatorio")]
+        [StringLength(50, ErrorMessage = "Campo obligatorio", MinimumLength = 3)]
+        [Display(Name ="Autor")]
+        public string Autor { get; set; }
+        
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [Display(Name = "Precio")]
+        public int Precio { get; set; }
+    }
+}
